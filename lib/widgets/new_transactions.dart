405a06670d4a6c753c8029567d41001214134842
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-15 20:53:37 -0400

    added validation that checks if amount is negative and can save data without having to click add transaction. added keyboard popup meant for numbers also

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -5,13 +5,26 @@ import 'package:flutter/material.dart';
  * enter our title and amount.
  */
 class NewTranscaction extends StatelessWidget {
-  
   // will get the final output instead of each keystroke
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
   final Function addNewTransaction;
   NewTranscaction(this.addNewTransaction);
+
+  void submitData() {
+    final entertedTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (entertedTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addNewTransaction(
+      entertedTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +41,20 @@ class NewTranscaction extends StatelessWidget {
               // customizing the text field
               decoration: InputDecoration(labelText: "Title"),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: "Amount"),
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              // we dont use the value but it's their just to make flutter happy
+              // the underscore means we get an argument but we don't care and don't use it
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text("Add Transaction"),
               textColor: Colors.purple,
-              onPressed: () {
-                addNewTransaction(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-                print("I was clicked");
-              },
+              onPressed: () => submitData,
             ),
           ],
         ),
