5ec685ade3d6f38fd29bc18bee9dbb7e159d4cc3
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-20 22:49:42 -0400

    added an image when there are no transactions. switched to using ListView.builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,48 +13,77 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // loop through the list of transactions
-        children: transaction.map((tx) {
-      return Card(
-          child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            decoration: BoxDecoration(
-                border: Border.all(
-                  // using the color that's passed down into the context by parent widget (main)
-                  color: Theme.of(context).primaryColor, 
-                width: 2)),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              // limits to two decimals places
-              '${tx.amount.toStringAsFixed(2)}',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                tx.title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              Text(
-                DateFormat().format(tx.date),
-                style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
+    return Container(
+        height: 300,
+        child: transaction.isEmpty
+            ? Column(
+                // loop through the list of transactions
+                children: <Widget>[
+                  Text(
+                    "No transactions added yet",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  // will add space between text and picture
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    /*
+                In order to make image smaller for smaller screen, wrapped 
+                the image in a container and set the height.
+                */
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                        fit: BoxFit.cover
+                        ),
+                  )
+                ],
               )
-            ],
-          )
-        ],
-      ));
-    }
-    ).toList());
+            : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 30
+                        ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              // using the color that's passed down into the context by parent widget (main)
+                              color: Theme.of(context).primaryColor,
+                              width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        // limits to two decimals places
+                        '${transaction[index].amount.toStringAsFixed(2)}',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaction[index].amount.toString(),
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          DateFormat().format(transaction[index].date),
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                              ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+                );
+              },
+              itemCount: transaction.length,
+              )
+            );
   }
 }
