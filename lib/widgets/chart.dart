e7a15ea2e2cfba4fb5532b258fc7da2ea0333ad7
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-25 21:41:09 -0400

    initial buid of chart widget. created method to get the transactions for the past 7 days

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -9,15 +9,21 @@ import '../model/transaction.dart';
 */
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
-
   Chart(this.recentTransactions);
+
   List<Map<String, Object>> get groupedTransactionsValues {
-    // creates a list of values. starts from 0 and goes to 6
-    // represents monday - sunday
-    // method will tell us which transactions were generated for each day
-    return List.generate(6, (index) {
+    /* creates a list of values. starts from 0 and goes to 6
+     represents monday - sunday
+     method will tell us which transactions were generated for each day
+     index -> 0-6
+    */
+    return List.generate(7, (index) {
+      /*
+        we need to find out the day of the week
+        and for each day get the total transactions
+      */
       final weekDay = DateTime.now().subtract(Duration(days: index));
-      var totalSum = 0.0;
+      double totalSum = 0.0;
       for (var i = 0; i < recentTransactions.length; i++) {
         // comparing by day, week, and month
         if (recentTransactions[i].date.day == weekDay.day &&
@@ -26,19 +32,23 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
       }
 
-      // returning map
-      // dateFormat.E() gives us exact day M T W
-      return {'day': DateFormat.E(weekDay), "amount": totalSum};
+      // print(DateFormat.E().format(weekDay));
+      /*
+       dateFormat.E() gives us shortcut for the weekday
+      */
+      return {'day': DateFormat.E().format(weekDay), "amount": totalSum};
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionsValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(10),
       child: Row(
         children: <Widget>[],
+        // holds the 7 bars for the days
       ),
     );
   }
