6f34805a0ed418fdd51b137e4ff497d3aaf4548f
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-08-02 19:28:35 -0400

    added method for data picker and ability to pick the data from a pop up calendar'

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 /**
  * This widget is for when we want to add a new transaction. It contains the TextField where we 
@@ -17,18 +18,19 @@ class NewTranscaction extends StatefulWidget {
 }
 
 class _NewTranscactionState extends State<NewTranscaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  void submitData() {
-    final entertedTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    final entertedTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (entertedTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
-  
-  // take the values and pass it to the function.
+
+    // take the values and pass it to the function.
     widget.addNewTransaction(
       entertedTitle,
       enteredAmount,
@@ -39,58 +41,86 @@ class _NewTranscactionState extends State<NewTranscaction> {
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    // shows datepicker on screen
+    // returns a future
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2020),
+            lastDate: DateTime.now()
+            // function executed once user choices the date
+            )
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
       child: Card(
-        elevation: 5,
+          elevation: 5,
           child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            // lets us input text
-            TextField(
-              // customizing the text field
-              decoration: InputDecoration(labelText: "Title"),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                // lets us input text
+                TextField(
+                  // customizing the text field
+                  decoration: InputDecoration(labelText: "Title"),
+                  controller: _titleController,
+                  onSubmitted: (_) => _submitData(),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: "Amount"),
+                  controller: _amountController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  // we dont use the value but it's their just to make flutter happy
+                  // the underscore means we get an argument but we don't care and don't use it
+                  onSubmitted: (_) => _submitData(),
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: <Widget>[
+                      // pushes date picker and date all the way to right and left
+                      Expanded(
+                          child: Text( _selectedDate == null  ? "No Date Chosen"  : 
+                         'Picked Date :  ${DateFormat.yMd().format(_selectedDate)}'
+                             ),
+                      ),
+                      FlatButton(
+                        textColor: Theme.of(context).primaryColor,
+                        child: Text(
+                                "Choose Date",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                           ,
+                        onPressed: _presentDatePicker,
+                      )
+                    ],
+                  ),
+                ),
+                RaisedButton(
+                  child: Text("Add Transaction"),
+                  color: Theme.of(context).primaryColor,
+                  // gets this color from the theme
+                  textColor: Theme.of(context).textTheme.button.color,
+                  onPressed: () => _submitData(),
+                ),
+              ],
             ),
-            TextField(
-              decoration: InputDecoration(labelText: "Amount"),
-              controller: amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              // we dont use the value but it's their just to make flutter happy
-              // the underscore means we get an argument but we don't care and don't use it
-              onSubmitted: (_) => submitData(),
-            ),
-            Container(
-              height: 70,
-                child: Row(
-                children: <Widget>[
-                  Text("No Date Choosen!"),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text("Choose Date", 
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),),
-                  onPressed: null,
-                  )
-              ],),
-            ),
-            RaisedButton(
-              child: Text("Add Transaction"),
-              color: Theme.of(context).primaryColor,
-              // gets this color from the theme
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: () => submitData(),
-            ),
-          ],
-        ),
-      )
-    ),
-  );
+          )),
+    );
   }
 }
