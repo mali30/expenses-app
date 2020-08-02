c2ee2f6a83b42803fa88a5537cb598cdef876509
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-08-02 19:28:58 -0400

    minor fix

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -33,7 +33,8 @@ class TransactionList extends StatelessWidget {
                 */
                     height: 200,
                     child: Image.asset('assets/images/waiting.png',
-                        fit: BoxFit.cover),
+                        fit: BoxFit.cover
+                        ),
                   )
                 ],
               )
