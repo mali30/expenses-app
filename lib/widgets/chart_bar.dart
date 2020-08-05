2e4d80f1093cb0cbb381ba71c0940b56bf7bbe8e
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-08-04 23:26:48 -0400

    added dynamic sizing thanks to LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-
   final String label;
   final double spendingAmount;
   final double spendingPercentage;
@@ -10,55 +9,61 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        // forces child into avialble space
-        // in our case makes text keep original size
-        Container(
-          // this makes bars align all across
-          height: 20,
-          child: FittedBox(
-            child: Text(
-              '\$${spendingAmount.toStringAsFixed(2)}')
-              )
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Column(
+          children: <Widget>[
+            // forces child into avialble space
+            // in our case makes text keep original size
+            Container(
+              // max height chart bar can take
+                height: constraint.maxHeight * 0.15,
+                child: FittedBox(
+                    child: Text('\$${spendingAmount.toStringAsFixed(2)}'))),
+            SizedBox(
+                height: constraint.maxHeight * 0.05,
             ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 70,
-          width: 10,
-          // allows you to place elements on top of each other
-          // really overlapping
-          child: Stack(
-            children: <Widget>[
-              // bottom most widget
-              // represents all of the 7 containers we see for bars
-              Container(
-                decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                color: Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(20),
-              )),
-              // represents the bar that fills up purple when passing in transactions
-              FractionallySizedBox(
-                  heightFactor: spendingPercentage,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ))
-            ],
-          ),
-        ),
-        // represents the little space between the day and the bar
-        SizedBox(
-          height: 4,
-        ),
-        // the day of the week
-        Text(label)
-      ],
+            Container(
+                height: constraint.maxHeight * 0.6,
+              width: 10,
+              // allows you to place elements on top of each other
+              // really overlapping
+              child: Stack(
+                children: <Widget>[
+                  // bottom most widget
+                  // represents all of the 7 containers we see for bars
+                  Container(
+                      decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+                  // represents the bar that fills up purple when passing in transactions
+                  FractionallySizedBox(
+                      heightFactor: spendingPercentage,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+            // represents the little space between the day and the bar
+            SizedBox(
+                height: constraint.maxHeight * 0.05,
+            ),
+            // the day of the week
+            Container(
+                height: constraint.maxHeight * 0.15,
+                child: FittedBox(
+                  child: Text(label)
+                  )
+                )
+          ],
+        );
+      },
     );
   }
 }
