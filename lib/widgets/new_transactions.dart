6ff29d0103bfb29b63c873ebf9a85a2fa0d4fa83
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-08-19 00:00:05 -0400

    changed button for choose date to iOS verion if using ioS device

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -104,7 +107,15 @@ class _NewTranscactionState extends State<NewTranscaction> {
                               ? "No Date Chosen"
                               : 'Picked Date :  ${DateFormat.yMd().format(_selectedDate)}'),
                         ),
-                        FlatButton(
+                        Platform.isIOS ? CupertinoButton(
+                           child: Text(
+                            "Choose Date",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: _presentDatePicker
+                        ) : FlatButton(
                           textColor: Theme.of(context).primaryColor,
                           child: Text(
                             "Choose Date",
